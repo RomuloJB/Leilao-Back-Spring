d0cae910c5702be31354347db78390539a853d29
RomuloJB <jordaobogoni@gmail.com>
2025-09-03 21:24:01 -0300

    add | feedback repository

---
+++ src/main/java/com/leilao/backend/repository/FeedbackRepository.java
@@ -0,0 +1,9 @@
+package com.leilao.backend.repository;
+
+import com.leilao.backend.model.Feedback;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
+}
